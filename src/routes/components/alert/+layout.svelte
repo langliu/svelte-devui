4b153e1cb8f16c75181e5303f403026f83024e6d
langliu <langliu1216@gmail.com>
2024-01-13 01:54:38 +0800

    feat: ✨ 优化Alert文档样式

--- src/routes/components/alert/+layout.svelte
+++ src/routes/components/alert/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Code2, Cable } from 'lucide-svelte';
 	let active: 'demo' | 'api' = 'demo';
 
 	function toggleActive(type: 'demo' | 'api') {
@@ -8,6 +9,7 @@
 
 <h1>Alert 警告</h1>
 <p class="doc-header">显示警告信息，需要用户关注的信息的组件。</p>
+
 <div class="tabs">
 	<div class="component-icon">
 		<img
@@ -15,31 +17,32 @@
 			alt="Alert Icon"
 		/>
 	</div>
-	<div class="nav-container">
-		<a
-			href="/components/alert/demo"
-			class="nav-slider"
-			tabindex={1}
-			on:click={() => toggleActive('demo')}
-		>
-			Demo
-		</a>
-		<a
-			href="/components/alert/api"
-			class="nav-slider"
-			tabindex={2}
-			on:click={() => toggleActive('api')}
-		>
-			API
-		</a>
-		<div class="nav-slider-animation" style:left={active === 'demo' ? '0px' : '50%'}></div>
-	</div>
+</div>
+<div class="nav-container">
+	<a
+		href="/components/alert/demo"
+		class="nav-slider"
+		tabindex={1}
+		on:click={() => toggleActive('demo')}
+	>
+		<Code2 size={16} />
+		Demo
+	</a>
+	<a
+		href="/components/alert/api"
+		class="nav-slider"
+		tabindex={2}
+		on:click={() => toggleActive('api')}
+	>
+		<Cable size={16} />
+		API
+	</a>
+	<div class="nav-slider-animation" style:left={active === 'demo' ? '0px' : '50%'}></div>
 </div>
 <slot />
 
 <style>
 	h1 {
-		position: sticky;
 		margin-top: 0;
 		margin-bottom: 16px;
 		font-size: 36px;
@@ -62,48 +65,8 @@
 		border-radius: 16px;
 		height: 176px;
 		background-size: cover;
-		background-repeat: no-repeat;
-		position: relative;
-		margin-bottom: 20px;
 		background-image: url('https://devui.design/components/assets/overview/banner/19.png');
-
-		& .nav-container {
-			font-size: var(--devui-font-size-lg);
-			display: flex;
-			align-items: stretch;
-			flex-wrap: nowrap;
-			border-radius: 0 0 16px 16px;
-			background: rgba(242, 242, 243, 0.6);
-			backdrop-filter: blur(20px);
-			width: 100%;
-
-			& .nav-slider {
-				height: 56px;
-				line-height: 56px;
-				text-align: center;
-				font-size: 18px;
-				color: var(--devui-text);
-				z-index: 2;
-				cursor: pointer;
-				flex: 1;
-				text-decoration: none;
-				background: transparent;
-			}
-
-			& .nav-slider-animation {
-				height: 56px;
-				line-height: 56px;
-				top: 0;
-				border-radius: 0 0 16px 16px;
-				box-shadow: 2px 2px 16px #0d275029;
-				position: absolute;
-				background-color: var(--devui-base-bg);
-				width: 50%;
-				transition:
-					left 0.3s cubic-bezier(0.5, 0.05, 0.5, 0.95),
-					width 0.3s cubic-bezier(0.5, 0.05, 0.5, 0.95);
-			}
-		}
+		position: relative;
 
 		& .component-icon {
 			position: absolute;
@@ -118,6 +81,48 @@
 				width: 100%;
 				height: 100%;
 			}
+		}
+	}
+	.nav-container {
+		font-size: var(--devui-font-size-lg);
+		display: flex;
+		align-items: stretch;
+		flex-wrap: nowrap;
+		border-radius: 0 0 16px 16px;
+		background: rgba(242, 242, 243, 0.6);
+		backdrop-filter: blur(20px);
+		width: 100%;
+		position: sticky;
+		top: 0;
+		margin-top: -56px;
+		margin-bottom: 20px;
+		z-index: 99;
+
+		& .nav-slider {
+			height: 56px;
+			line-height: 56px;
+			text-align: center;
+			font-size: 18px;
+			color: var(--devui-text);
+			z-index: 2;
+			cursor: pointer;
+			flex: 100;
+			text-decoration: none;
+			background: transparent;
+		}
+
+		& .nav-slider-animation {
+			height: 56px;
+			line-height: 56px;
+			top: 0;
+			border-radius: 0 0 16px 16px;
+			box-shadow: 2px 2px 16px #0d275029;
+			position: absolute;
+			background-color: var(--devui-base-bg);
+			width: 50%;
+			transition:
+				left 0.3s cubic-bezier(0.5, 0.05, 0.5, 0.95),
+				width 0.3s cubic-bezier(0.5, 0.05, 0.5, 0.95);
 		}
 	}
 </style>
